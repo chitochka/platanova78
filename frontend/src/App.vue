f523c293fb58df392ae75a09f3de94f0cf86cb3a
chitochka <javascript@post.cz>
2024-08-07 22:56:10 +0200

    dark & light Theme | SideBar | login Page

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,19 +1,41 @@
-
 <template>
-  <v-app id="inspire">
-    <v-navigation-drawer >
-      <nav>        SideBar      </nav>
-    </v-navigation-drawer>
+  <v-app :theme="_theme">
 
     <v-app-bar>
-      <v-app-bar-nav-icon >Icon</v-app-bar-nav-icon>
-      <v-app-bar-title> Title </v-app-bar-title>
+      <v-app-bar-nav-icon @click.stop="_drawer = !_drawer"></v-app-bar-nav-icon>
+      <v-app-bar-title> Registrace noveho uzivatele </v-app-bar-title>
+      <v-spacer></v-spacer>
+      <v-btn :prepend-icon="_theme === 'light' ? 'mdi-weather-sunny' : 'mdi-weather-night'" @click="changeTheme">
+      </v-btn>
     </v-app-bar>
 
-    <v-main>
-        <Login></Login>   
+    <v-navigation-drawer v-model="_drawer">
+      <nav> SideBar </nav>
+    </v-navigation-drawer>
+
+    <v-main d-flex align-center justify-center>
+      <Login></Login>
     </v-main>
     <AppFooter />
-
   </v-app>
 </template>
+
+
+
+<script>
+export default {
+  data() {
+    return {
+      _theme: 'light',
+      _drawer: false
+    }
+  },
+  methods: {
+    changeTheme() {
+      this._theme = this._theme === 'light' ? 'dark' : 'light'
+    }
+  }
+}
+
+
+</script>
--- frontend/src/components/Login.vue
+++ frontend/src/components/Login.vue
@@ -1,90 +1,115 @@
 <template>
-  <!--    TEST  VUelidate   -->
+    <v-container style="height: 80%">
+        <v-row align="center" style="height: 100%;" no-gutters>
+            <v-col>
 
-  <!-- <input type="text" :value="name" @input="setName">
-  test = [{{ test }}]
+                <v-card class="mx-auto" min-width="500" max-width="800">
+                    <v-card-title class="text-h6 font-weight-regular justify-space-between">
+                        <span>{{ currentTitle }}</span>
+                        <v-avatar color="primary" size="24">{{ step }}</v-avatar>
+                    </v-card-title>
+                    <form>
 
-  <v-text-field v-model="test" :error-messages="v$.test.$errors.map(e => e.$message)" label="E-mail"
-      @input="v$.test.$touch" @blur="v$.test.$touch" required></v-text-field>
-  {{ v$.test }}
-  <hr>
-  error = {{ v$.test.$errors }} -->
+                        <v-window v-model="step">
 
-<!-- --------------------- -->
+                            <!-- 1 -->
+                            <v-window-item :value="1">
+                                <v-card-text>
+                                    <v-text-field v-model.trim="email" label="Zadejte vas email?"
+                                        :error-messages="v$.email.$errors.map(e => e.$message)" required
+                                        @input="v$.email.$touch" variant="outlined"
+                                        prepend-inner-icon="mdi-email-outline"
+                                    >
+                                    </v-text-field>
+                                    <span class="text-caption text-grey-darken-1 warning"> Zadejte , prosim, Vas email</span>
+                                </v-card-text>
+                            </v-window-item>
 
-  <v-layout align-center justify-center>
-      <v-card class="mx-auto" min-width="500">
-          <v-card-title class="text-h6 font-weight-regular justify-space-between">
-              <span>{{ currentTitle }}</span>
-              <v-avatar color="primary" size="24">{{ step }}</v-avatar>
+                            <!-- 2 -->
 
-          </v-card-title>
-          <form>
+                            <v-window-item :value="2">
+                                <v-card-text>
+                                    <v-text-field label="Password" variant="outlined"
+                                        required  
+                                        :type="_passwordVisible ? 'text' : 'password'"
+                                        :append-inner-icon="_passwordVisible ? 'mdi-eye-off' : 'mdi-eye'"
+                                        prepend-inner-icon="mdi-lock-outline"
+                                        @click:append-inner="_passwordVisible = !_passwordVisible"
+                                        ></v-text-field>
+                                    <v-text-field label="Confirm Password" variant="outlined"
+                                        required  
+                                        :type="_passwordVisible ? 'text' : 'password'"
+                                        
+                                        ></v-text-field>
+                                    <span class="text-caption text-grey-darken-1"> Please enter a password for your
+                                        account
+                                    </span>
+                                </v-card-text>
+                            </v-window-item>
 
-              <v-window v-model="step">
-                  <v-window-item :value="1">
-                      <v-card-text>
-                          <!-- <v-text-field v-model="dataUser.email"
-                          :error-messages="v$.dataUser.email.$errors.map(e => e.$message)" label="E-mail" required
-                          @input="v$.email.$touch" @blur="v$.email.$touch"></v-text-field> -->
-                          <v-text-field v-model="email" :error-messages="v$.email.$errors.map(e => e.$message)"
-                              required></v-text-field>
-                          <!-- <v-text-field label="Password" type="password"></v-text-field> -->
-                          <!-- <v-text-field label="Confirm Password" type="password"  v-if="true"></v-text-field> -->
-
-                          <!-- <v-text-field label="Email" placeholder="john@google.com"></v-text-field> -->
-                          <span class="text-caption text-grey-darken-1"> Zadejte , prosim, Vas email</span>
-                          <span class="text-caption text-grey-darken-1"> {{ test }}</span>
-                      </v-card-text>
-                  </v-window-item>
-
-                  <v-window-item :value="2">
-                      <v-card-text>
-
-                          <v-text-field v-model="name" :error-messages="v$.name.$errors.map(e => e.$message)"
-                              :counter="10" label="Jak se jmenujete?" required @input="v$.name.$touch"
-                              @blur="v$.name.$touch"></v-text-field>
-                          <v-text-field v-model="telefon" :error-messages="v$.name.$errors.map(e => e.$message)"
-                              :counter="10" label="Telefon:" required @input="v$.name.$touch"
-                              @blur="v$.name.$touch"></v-text-field>
-                          <v-select v-model="apartNum" :items="Array.from({ length: 13 }, (v, i) => i + 1)"
-                              :error-messages="v$.apartNum.$errors.map(e => e.$message)" label="Ze ktereho jste bytu? "
-                              required @change="v$.apartNum.$touch" @blur="v$.apartNum.$touch"></v-select>
-
-                          <!-- <v-checkbox v-model="state.checkbox" :error-messages="v$.checkbox.$errors.map(e => e.$message)"             label="Jste vlastnik bytu??" required @change="v$.checkbox.$touch" @blur="v$.checkbox.$touch"></v-checkbox> -->
+                            <!-- 3  -->
 
 
-                          <!-- <v-text-field label="Password" type="password"></v-text-field>
-                  <v-text-field label="Confirm Password" type="password"></v-text-field>
-                  <span class="text-caption text-grey-darken-1">                        Please enter a password for your account                    </span> -->
-                      </v-card-text>
-                  </v-window-item>
+                            <v-window-item :value="3">
+                                <v-card-text>
 
-                  <v-window-item :value="3">
-                      <div class="pa-4 text-center">
+                                    <v-text-field v-model="firstName"
+                                        :error-messages="v$.firstName.$errors.map(e => e.$message)" label="Jmeno"
+                                        required @input="v$.firstName.$touch" @blur="v$.firstName.$touch"
+                                        variant="outlined">
+                                    </v-text-field>
+                                    <v-text-field v-model="lastName"
+                                        :error-messages="v$.lastName.$errors.map(e => e.$message)" label="Primeni"
+                                        required @input="v$.lastName.$touch" @blur="v$.lastName.$touch" variant="outlined">
+                                    </v-text-field>
+                                    <v-text-field v-model="telefon"
+                                        :error-messages="v$.telefon.$errors.map(e => e.$message)" label="Telefon:" required
+                                        @input="v$.telefon.$touch" @blur="v$.telefon.$touch" variant="outlined">
+                                    </v-text-field>
+                                    <div class="d-flex ga-2">
+                                        <v-select v-model="apartNum"
+                                            :items="Array.from({ length: 13 }, (v, i) => i + 1)"
+                                            :error-messages="v$.apartNum.$errors.map(e => e.$message)"
+                                            label="Ze ktereho jste bytu? " required @change="v$.apartNum.$touch"
+                                            @blur="v$.apartNum.$touch" variant="outlined">
+                                        </v-select>
 
-                          <h3 class="text-h6 font-weight-light mb-2">
-                              Welcome to Vuetify
-                          </h3>
-                          <span class="text-caption text-grey">Thanks for signing up!</span>
-                      </div>
-                  </v-window-item>
-              </v-window>
-          </form>
-          <!-- <v-divider></v-divider> -->
+                                        <v-checkbox v-model="vlastnik"
+                                            :error-messages="v$.vlastnik.$errors.map(e => e.$message)"
+                                            label="Jste vlastnik bytu??" @change="v$.vlastnik.$touch"
+                                            @blur="v$.vlastnik.$touch">
+                                        </v-checkbox>
+                                    </div>
+                                </v-card-text>
+                            </v-window-item>
 
-          <v-card-actions>
-              <v-btn v-if="step > 1" variant="text" @click="step--">
-                  Back
-              </v-btn>
-              <v-spacer></v-spacer>
-              <v-btn v-if="step < 3" color="primary" variant="flat" :loading="loading" @click="nextStep(step)"
-                  :disabled="this.v$.email.$error">
-                  Next
-              </v-btn>
-          </v-card-actions>
-      </v-card>
-  </v-layout>
+                            <!-- 4 -->
+
+                            <v-window-item :value="4">
+                                <div class="pa-4 text-center">
+
+                                    <h3 class="text-h6 font-weight-light mb-2"> Dekujeme za registrace </h3>
+                                    <span class="text-caption text-grey"> Za chvili budete prihlaseni </span>
+                                </div>
+                            </v-window-item>
+                        </v-window>
+                    </form>
+                    <v-divider></v-divider>
+
+                    <v-card-actions>
+                        <v-btn v-if="step > 1" variant="text" @click="step--">
+                            Back
+                        </v-btn>
+                        <v-spacer></v-spacer>
+                        <v-btn v-if="step < 4" color="primary" variant="flat" :loading="loading" @click="nextStep(step)"
+                            :disabled="this.v$.email.$error">
+                            Next
+                        </v-btn>
+                    </v-card-actions>
+                </v-card>
+            </v-col>
+        </v-row>
+    </v-container>
 </template>
 <script>
 
@@ -94,56 +119,58 @@ import { useVuelidate } from '@vuelidate/core'
 import { required, numeric, email } from '@vuelidate/validators'
 
 export default {
-  setup: () => ({ v$: useVuelidate() }),
-  data: () => ({
-      name: '',
-      step: 1,
-      loading: false,
-      email: '',
-      password: "",
-      firstName: "",
-      lastName: "",
-      apartNum: null,
-      telefon: "",
-      vlastnik: false,
-      test: '',
-      items: Array.from({ length: 13 }, (v, i) => i + 1)
-  }),
-  validations() {
-      return {
-          name:  { required },
-          email: { required, email },
-          password: { required },
-          firstName: { required },
-          lastName: { required },
-          apartNum: { required, numeric },
-          telefon: { required },
-          vlastnik: { required },
-          test: { required, email },
-      }
-  },
-  computed: {
-      currentTitle() {
-          switch (this.step) {
-              case 1: return 'Registrace noveho uzivatele '
-              case 2: return 'Create a password'
-              default: return 'Account created'
-          }
-      },
-  },
-  methods: {
-      nextStep: function () {
-          this.loading = true;
-          setTimeout(() => {
-              this.loading = false
-              this.step++
+    setup: () => ({ v$: useVuelidate() }),
+    data: () => ({
 
-          }, 1500);
+        step: 1,
+        loading: false,
+        email: '',
+        password: "",
+        firstName: "",
+        lastName: "",
+        apartNum: null,
+        telefon: "",
+        vlastnik: false,
+        items: Array.from({ length: 13 }, (v, i) => i + 1),
+        _passwordVisible: false
+    }),
+    validations() {
+        return {
+            email: { required, email },
+            password: { required },
+            firstName: { required },
+            lastName: { required },
+            apartNum: { required, numeric },
+            telefon: { required, numeric },
+            vlastnik: { required },
+        }
+    },
+    computed: {
+        currentTitle() {
+            switch (this.step) {
+                case 1: return 'New user registration'
+                case 2: return 'Create a password'
+                case 3: return 'Enter your data'
+                default: return 'Account created'
+            }
+        },
+    },
+    methods: {
+        nextStep: function () {
+            this.loading = true;
+            setTimeout(() => {
+                this.loading = false
+                this.step++
 
-      }
-  },
-  mounted() {
-      console.log('mount')
-  }
+            }, 1500);
+
+        }
+    },
+    mounted() {
+        console.log('mount')
+    }
 }
 </script>
+
+
+<style scope></style>

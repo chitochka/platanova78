93716aae55207966eb20f84bc03503b482ab8b82
chitochka <javascript@post.cz>
2024-08-14 22:31:07 +0200

    menu jazyky

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,16 +1,20 @@
 <template>
   <v-app :theme="_theme">
-
     <v-app-bar>
       <v-app-bar-nav-icon @click.stop="_drawer = !_drawer"></v-app-bar-nav-icon>
       <v-app-bar-title> Registrace noveho uzivatele </v-app-bar-title>
       <v-spacer></v-spacer>
-      <v-btn :prepend-icon="_theme === 'light' ? 'mdi-weather-sunny' : 'mdi-weather-night'" @click="changeTheme">
-      </v-btn>
+      <v-btn :prepend-icon="_theme === 'light' ? 'mdi-weather-sunny' : 'mdi-weather-night'"
+        @click="changeTheme"
+      ></v-btn>
+      <v-spacer></v-spacer>
+      <div>
+        <v-select :item-props="itemProps" :items="items" ></v-select>
+      </div>
     </v-app-bar>
 
     <v-navigation-drawer v-model="_drawer">
-      <nav> SideBar </nav>
+      <nav>SideBar</nav>
     </v-navigation-drawer>
 
     <v-main d-flex align-center justify-center>
@@ -26,16 +30,35 @@
 export default {
   data() {
     return {
-      _theme: 'light',
-      _drawer: false
-    }
+      _theme: "light",
+      _drawer: false,
+      items: [
+        {
+          name: 'Cestina',
+          icon: 'mdi-weather-sunny',
+        },
+        {
+          name: 'Русский',
+          icon: 'mdi-weather-sunny',
+        },
+        {
+          name: 'English',
+          icon: 'mdi-weather-sunny',
+        }
+
+      ]
+    };
   },
   methods: {
+    itemProps(item) {
+        return {
+          title: item.name,
+          appendIcon: item.icon,
+        }
+      },
     changeTheme() {
-      this._theme = this._theme === 'light' ? 'dark' : 'light'
-    }
-  }
-}
-
-
+      this._theme = this._theme === "light" ? "dark" : "light";
+    },
+  },
+};
 </script>

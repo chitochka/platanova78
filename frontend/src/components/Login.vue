2f7dd781f7e1f1081f2ca67d4bb12e5488a07536
chitochka <javascript@post.cz>
2024-08-31 11:13:01 +0200

    validation

--- frontend/src/components/Login.vue
+++ frontend/src/components/Login.vue
@@ -16,12 +16,16 @@
                             <v-window-item :value="1">
                                 <v-card-text>
                                     <v-text-field v-model.trim="email" label="Zadejte vas email?"
-                                        :error-messages="v$.email.$errors.map(e => e.$message)" required
-                                        @input="v$.email.$touch" variant="outlined"
-                                        prepend-inner-icon="mdi-email-outline"
-                                    >
+                                        :error-messages="v$.email.$errors.map(e => e.$message)" required  
+                                        @input="v$.email.$touch" variant="outlined" @keypress="this.alert=false" 
+                                        prepend-inner-icon="mdi-email-outline"  clearable  :class="this.alert?'err':''" 
                                     </v-text-field>
-                                    <span class="text-caption text-grey-darken-1 warning"> Zadejte , prosim, Vas email</span>
+                                    <!-- <span v-if="!v$.email.$errors.length" class="text-caption text-grey-darken-1 warning "> Zadejte , prosim, Vas email</span> -->
+                                    <v-alert  
+                                            border-color="error"  border="start"
+                                            v-model="alert"  closable  text='Данный Email уже зарегистрирован' type="error" density="comfortable" 
+                                            variant="tonal" elevation="10"
+                                            ></v-alert>
                                 </v-card-text>
                             </v-window-item>
 
@@ -29,14 +33,16 @@
 
                             <v-window-item :value="2">
                                 <v-card-text>
-                                    <v-text-field label="Password" variant="outlined"
+                                    <v-text-field v-model="password" @input="v$.password.$touch" :error-messages="v$.password.$errors.map(e => e.$message)"
+                                        label="Password" variant="outlined" clearable
                                         required  
                                         :type="_passwordVisible ? 'text' : 'password'"
                                         :append-inner-icon="_passwordVisible ? 'mdi-eye-off' : 'mdi-eye'"
                                         prepend-inner-icon="mdi-lock-outline"
                                         @click:append-inner="_passwordVisible = !_passwordVisible"
                                         ></v-text-field>
-                                    <v-text-field label="Confirm Password" variant="outlined"
+                                    <v-text-field v-model="confirmPassword"  @input="v$.confirmPassword.$touch"  :error-messages="v$.confirmPassword.$errors.map(e => e.$message)"
+                                        label="Confirm Password" variant="outlined" clearable
                                         required  
                                         :type="_passwordVisible ? 'text' : 'password'"
                                         
@@ -49,23 +55,30 @@
 
                             <!-- 3  -->
 
-
                             <v-window-item :value="3">
                                 <v-card-text>
 
                                     <v-text-field v-model="firstName"
                                         :error-messages="v$.firstName.$errors.map(e => e.$message)" label="Jmeno"
                                         required @input="v$.firstName.$touch" @blur="v$.firstName.$touch"
-                                        variant="outlined">
+                                        variant="outlined" clearable> 
                                     </v-text-field>
                                     <v-text-field v-model="lastName"
                                         :error-messages="v$.lastName.$errors.map(e => e.$message)" label="Primeni"
-                                        required @input="v$.lastName.$touch" @blur="v$.lastName.$touch" variant="outlined">
+                                        required @input="v$.lastName.$touch" @blur="v$.lastName.$touch" variant="outlined" clearable>
                                     </v-text-field>
                                     <v-text-field v-model="telefon"
                                         :error-messages="v$.telefon.$errors.map(e => e.$message)" label="Telefon:" required
-                                        @input="v$.telefon.$touch" @blur="v$.telefon.$touch" variant="outlined">
+                                        @input="v$.telefon.$touch" @blur="v$.telefon.$touch" variant="outlined" clearable :class="this.alert?'err':''"
+                                        @keypress="this.alert=false"   persistent-counter
+                                        >
                                     </v-text-field>
+
+                                    <v-alert  style="z-index:9999"
+                                            border-color="error"  border="start"
+                                            v-model="alert"  closable  text='Данный Email уже зарегистрирован' type="error" density="comfortable" 
+                                            variant="tonal" elevation="10"  
+                                            ></v-alert>                                    <br v-if="alert">
                                     <div class="d-flex ga-2">
                                         <v-select v-model="apartNum"
                                             :items="Array.from({ length: 13 }, (v, i) => i + 1)"
@@ -80,6 +93,7 @@
                                             @blur="v$.vlastnik.$touch">
                                         </v-checkbox>
                                     </div>
+
                                 </v-card-text>
                             </v-window-item>
 
@@ -102,11 +116,12 @@
                         </v-btn>
                         <v-spacer></v-spacer>
                         <v-btn v-if="step < 4" color="primary" variant="flat" :loading="loading" @click="nextStep(step)"
-                            :disabled="this.v$.email.$error">
+                            :disabled="this.disabled || this.alert">
                             {{$t('components.btnNext')}}
                         </v-btn>
                     </v-card-actions>
                 </v-card>
+ 
             </v-col>
         </v-row>
     </v-container>
@@ -114,7 +129,7 @@
 <script>
 
 import { useVuelidate } from '@vuelidate/core'
-import { required, numeric, email } from '@vuelidate/validators'
+import { required, numeric, email,sameAs, minLength,alpha } from '@vuelidate/validators'
 import {useI18n} from 'vue-i18n'
 import UserDataService from '../services/UserDataService'
 
@@ -125,11 +140,12 @@ export default {
         return { v$ , t } 
     },
     data: () => ({
-
+        alert:false,
         step: 1,
         loading: false,
         email: '',
         password: "",
+        confirmPassword:'',
         firstName: "",
         lastName: "",
         apartNum: null,
@@ -140,12 +156,17 @@ export default {
     }),
     validations() {
         return {
-            email: { required, email },
-            password: { required },
-            firstName: { required },
-            lastName: { required },
+            email: { required, email    },
+            password: { required,minLength: minLength(5) },
+            confirmPassword: {
+                required,
+                minLength: minLength(5),
+                sameAs: sameAs(this.password),
+            },
+            firstName: {required, alpha, minLength: minLength(2) },
+            lastName: { required, alpha, minLength: minLength(2) },
             apartNum: { required, numeric },
-            telefon: { required, numeric },
+            telefon: { required, numeric, minLength: minLength(9) },
             vlastnik: { required },
         }
     },
@@ -158,25 +179,84 @@ export default {
                 default: return 'Account created'
             }
         },
+        disabled (){
+            switch (this.step){
+                case 1: return  this.v$.email.$invalid ; break
+                case 2: return (this.v$.password.$invalid || this.v$.confirmPassword.$invalid) ;  break;
+                case 3: return  (this.v$.firstName.$invalid || this.v$.lastName.$invalid ||   this.v$.telefon.$invalid ||   this.v$.apartNum.$invalid  )  ; break
+                                
+            }
+        }
+
     },
     methods: {
-        nextStep: function () {
-            this.loading = true;
-            UserDataService.getEmail({email:this.email})
-            .then( res => {
-                this.loading = false
-                this.step++
-            }).catch(e => {
-                this.loading = false
-                console.log(e);
-            });
+        update(a){console.log(a)},
+        nextStep: function (e) {
+            switch (e) {
+                case 1:  
+                    this.loading = true;
+                    UserDataService.getEmail({email:this.email.toLocaleLowerCase()})
+                    .then( (res, req) => {
+                        this.loading = false
+                        if (res.data.exist) {
+                            this.alert = true
+                        } else {
+                            this.step++
+                        }
+                        console.log(res.data)
+
+                    }).catch(e => {
+                        this.loading = false
+                        console.log(e);
+                    });
+                    break;
+                case 2 : this.step++; break
+                case 3 : 
+                    UserDataService.getEmail({telefon:this.telefon.toLocaleLowerCase()})
+                        .then( (res, req) => {
+                            this.loading = false
+                            if (res.data.exist) {
+                                this.alert = true
+                            } else {
+
+
+                                UserDataService.create({email:this.email})
+                                    .then( (res, req) => {
+                                        this.loading = false
+                                        this.step++
+                                    }).catch(e => {
+                                        this.loading = false
+                                        console.log(e);
+                                    });
+
+
+                                this.step++
+                            }
+                        }).catch(e => {
+                            this.loading = false
+                            console.log(e);
+                        });
+                        break;
+
+            }
         }
     },
     mounted() {
         console.log('mount')
+        window.vv =this
     }
 }
 </script>
 
 
-<style scope></style>
+<style scope>
+.err  .v-field__input {
+    color: #b20827 !important;
+    /* border : 1px solid red; */
+}
+.v-text-field {
+    margin-bottom: 5px;
+}
+
+
+</style>

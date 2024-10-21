1af7c7504f0dea273552673d9fb270c443de9c1a
chitochka <javascript@post.cz>
2024-10-21 17:41:34 +0200

    .env

--- backend/config/db.config.js
+++ backend/config/db.config.js
@@ -1,3 +1,11 @@
+var  env = require('dotenv').config()
+const DB_HOME_PC= process.env.DB_HOME_PC 
+
+console.log('DB = ' ,  DB_HOME_PC ? "HOME LOCALE DB ":" ONLINE ")
+const url = DB_HOME_PC 
+  ? "mongodb://localhost:27017/platanova78"
+  : "mongodb+srv://alex:alex@cluster0.zyelpcb.mongodb.net/platanova78"
+
 module.exports = {
-  url: "mongodb://localhost:27017/platanova78",
+  url
 };
